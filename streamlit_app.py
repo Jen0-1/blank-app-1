26c7de5f98293cce4f63f55514531d814fc0394c
Jen0-1 <jengundersen07@gmail.com>
2025-02-20 22:46:53 +0000

    titulo en español y botones dinamicos

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 MI NUEVA APP")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+
+cantidad = st.slider('selecciona la cantida')
